6dd6e0c52c8e6eaa4d9e3d1973c7a20fedf2d533
leon3318 <ya1483leon@gmail.com>
2025-05-19 21:34:17 +0300

    make units

---
+++ lesson_1/practice.py
@@ -0,0 +1,35 @@
+# 1
+number_1 = int(input("Введите первое целое число: "))
+number_2 = int(input("Введите второе целое число: "))
+
+sum = int(number_1 + number_2)
+input(sum)
+
+# 2
+str_1 = input("Введите строку: ")
+result = int(str_1)
+input(result)
+
+# 3
+
+number_3 = int(input("Введите первое целое число: "))
+number_4 = int(input("Введите второе целое число: "))
+
+result_2 = number_3 - number_4
+input(result_2)
+
+# 4
+number_5 = float(input("Введите первое дробное число: "))
+number_6 = float(("Введите второе дробное число: "))
+
+result_3 = number_5 * number_6
+input(result_3)
+
+# 5
+
+# 6
+str_2 = str(input("Введите первую строку: "))
+str_3 = str(input("Введите вторую строку: "))
+
+result_4 = (str_2 + " " + str_3)
+input(result_4)
