b286e3ccfbfd842cf0495d44dcac0f71b7a59426
leon3318 <ya1483leon@gmail.com>
2025-05-19 21:46:58 +0300

    completed task

--- lesson_1/practice.py
+++ lesson_1/practice.py
@@ -2,13 +2,13 @@
 number_1 = int(input("Введите первое целое число: "))
 number_2 = int(input("Введите второе целое число: "))
 
-sum = int(number_1 + number_2)
-input(sum)
+sum = (number_1 + number_2)
+print(sum)
 
 # 2
 str_1 = input("Введите строку: ")
 result = int(str_1)
-input(result)
+print(result)
 
 # 3
 
@@ -16,20 +16,32 @@ number_3 = int(input("Введите первое целое число: "))
 number_4 = int(input("Введите второе целое число: "))
 
 result_2 = number_3 - number_4
-input(result_2)
+print(result_2)
 
 # 4
 number_5 = float(input("Введите первое дробное число: "))
 number_6 = float(("Введите второе дробное число: "))
 
 result_3 = number_5 * number_6
-input(result_3)
+print(result_3)
+
+result_32 = number_5 / number_6
+print(result_32)
 
 # 5
+number_7 = int(input("Введите число: "))
+print(str(number_7))
 
 # 6
 str_2 = str(input("Введите первую строку: "))
 str_3 = str(input("Введите вторую строку: "))
 
 result_4 = (str_2 + " " + str_3)
-input(result_4)
+print(result_4)
+
+# 7
+str_4 = str(input("Введите строку: "))
+number_8 = int(input("Введите число: "))
+result_5 = int(str_4 + number_8)
+
+print(result_5)
