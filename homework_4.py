b7079dc859f79e69dfada8d8880a3bcc7c43f77a
leon3318 <ya1483leon@gmail.com>
2025-06-26 02:03:49 +0300

    -make homework

--- homework_4.py
+++ homework_4.py
@@ -34,18 +34,39 @@ for animal in animals:
         herbivores_counter += 1
 print(f"Хищников: {predators_counter}, Травоядных: {herbivores_counter}")
 
-# 4
+4
 highs = [100, 110, 105, 120, 125, 130, 128]
-highest = []
-current = 0
-the_highest = 0
+the_highest = []
+current = []
 
+if len(highs) > 0:
+    for i in range(1, len(highs)):
+        if highs[i] > highs[i-1]:
+            current.append(highs[i])
+            if len(current) > len(the_highest):
+                the_highest = current.copy()
+        else:
+            current.clear()
+
+    print(f"Самый длинный непрерывный участок: {the_highest}")
+else:
+    print("Альпинист не поднимался!")
 
 # 5
 
-dif_lengths = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+dif_lengths = [[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]]
 main_sum = 0
 submain_sum = 0
+
+for item in range(len(dif_lengths)):
+    main_sum += dif_lengths[item][item]
+    submain_sum += dif_lengths[item][len(dif_lengths)-1-item]
+    # submain_sum += dif_lengths[item-1][item-1] Можно ли так решить было?
+
+print(
+    f"Сумма главной диагонали: {main_sum}. Сумма побочной диагонали: {submain_sum}.")
 
 # 6
 
@@ -90,10 +111,19 @@ sorted_racer = sorted(racer, key=lambda x: x[1])
 print(sorted_racer)
 
 
-# 10
-pupils = [('Анна', [5, 5, 4]), ('Борис', [4, 3, 5]), ('Виктор', [5, 5, 5])]
-best_middle_note = 0
+# # 10
+results = [('Анна', [5, 5, 4]), ('Борис', [4, 3, 5]), ('Виктор', [5, 5, 5])]
+dict_results = dict(results)
 
+avg_score = 0
+avg = {k: round(sum(v)/len(v), 2) for k, v in dict_results.items()}
+
+inv_dict = {v: k for k, v in avg.items()}
+
+for key, value in inv_dict.items():
+    if avg_score < key:
+        avg_score = key
+print(inv_dict.get(avg_score))
 
 # 11
 my_friends = ['Аня', 'Петя', 'Вася']
@@ -132,7 +162,62 @@ friend_books_set = set(friend_books)
 difference_books = my_books_set ^ friend_books_set
 print(difference_books)
 
-# 15
+# # 15
+languages = [{'английский', 'испанский'}, {
+    'английский', 'французский'}, {'английский', 'немецкий'}]
+
+posol_1 = languages[0]
+
+for language in range(len(languages)):
+    union = posol_1 & languages[language]
+print(union)
+
+# # 16
+names = ['Маша', 'Петя', 'Вася', 'Маша', 'Петя', 'Маша']
+
+dict_names = {name: names.count(name) for name in set(names)}
+print(dict_names)
+
+# 17
+grades = {'Иванов': 55, 'Петров': 72, 'Сидоров': 48}
+
+for name in grades.keys():
+    if grades[name] < 50:
+        grades[name] += 5
+    elif 50 <= grades[name] < 60:
+        grades[name] += 5
+
+print(grades)
+
+# 18
+inventory = {'ручка': 20, 'книга': 800, 'рюкзак': 1500}
+filtered_inventory = {}
+
+for item, price in inventory.items():
+    if price >= 500:
+        filtered_inventory[item] = price
+
+print(filtered_inventory)
+
+# 19
+capitals = {'Россия': 'Москва', 'Франция': 'Париж', 'Италия': 'Рим'}
+inverted_capitals = {}
+
+for country, city in capitals.items():
+    inverted_capitals[city] = country
+
+print(inverted_capitals)
+
+# 20
+data = {'user': {'name': 'Alice', 'profile': {'theme': 'dark'}}}
+path = 'user.profile.theme'
 
 
-# 16
+keys = path.split('.')
+
+current = data
+
+for key in keys:
+    current = current[key]
+
+print(current)
